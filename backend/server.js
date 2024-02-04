8be35a6e35f834fdf240780da7d10e7c9a6bf582
vishal <vishalpote9090@gmail.com>
2024-02-04 16:55:51 +0530

    add client dist folder

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,21 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connection from './database/mongo.js';
 import router from './routes/route.js';
+import path from 'path';
 dotenv.config();
 
 const app= express();
-// app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, './frontend/dist')));
 app.use(express.json());
 app.use(cors());
 
 const port=process.env.PORT || 8060;
 
 connection()
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/dist/index.html')
+})
 
 app.use('/api/v1',router)
    
